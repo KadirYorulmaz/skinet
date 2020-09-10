9b510385d3daec8cdd696c6f3663199fb1af5a12
kadir yorulmaz <kadiryorulmaz1@hotmail.com>
2020-09-10 22:05:18 +0200

    done API Architecture

--- Infrastructure/Data/Migrations/20200828155210_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20200828155210_InitialCreate.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20200825194138_InitialCreate")]
+    [Migration("20200828155210_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -15,27 +15,14 @@ namespace Infrastructure.Data
         {
             try
             {
-                  if (!context.Products.Any())
-                {
-                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
-
-                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
-
-                    foreach (var item in products)
-                    {
-                        context.Products.Add(item);
-                    }
-
-                    await context.SaveChangesAsync();
-                }
-
                 if (!context.ProductBrands.Any())
                 {
-                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brandsData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
 
                     var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
-                    
-                    foreach(var item in brands)
+
+                    foreach (var item in brands)
                     {
                         context.ProductBrands.Add(item);
                     }
@@ -43,9 +30,25 @@ namespace Infrastructure.Data
                     await context.SaveChangesAsync();
                 }
 
-                       if (!context.Products.Any())
+                if (!context.ProductTypes.Any())
                 {
-                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var typesData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    foreach (var item in types)
+                    {
+                        context.ProductTypes.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Products.Any())
+                {
+                    var productsData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
 
                     var products = JsonSerializer.Deserialize<List<Product>>(productsData);
 
@@ -56,6 +59,7 @@ namespace Infrastructure.Data
 
                     await context.SaveChangesAsync();
                 }
+
             }
             catch (Exception ex)
             {
