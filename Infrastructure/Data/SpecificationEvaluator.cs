0293fc448ef241f529212d96666fa0ba508076bf
kadir yorulmaz <kadiryorulmaz1@hotmail.com>
2020-10-04 13:23:44 +0200

    End of section 6

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -9,8 +9,24 @@ namespace Infrastructure.Data
     {
         public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec){
             var query = inputQuery;
+
             if(spec.Criteria != null){
                 query = query.Where(spec.Criteria); // p => p.ProductTypeId == id
+            }
+
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy); // p => p.ProductTypeId == id
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending); // p => p.ProductTypeId == id
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
             }
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
